3f67188b53c18e755f2a11fc3aebc73988ccf9b5
pazpok <mathebossdu22@hotmail.fr>
2019-01-18 09:57:23 +0100

    Install WP + ACF

---
+++ wp-content/themes/pazpok/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<? bloginfo( 'pingback_url' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php wp_title(''); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    
