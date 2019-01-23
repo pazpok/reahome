aa54e5ea695fd17416863abc82ff3178617245cc
pazpok <mathebossdu22@hotmail.fr>
2019-01-23 09:32:34 +0100

    css home.php + single.php

--- wp-content/themes/pazpok/home.php
+++ wp-content/themes/pazpok/home.php
@@ -1,20 +1,23 @@
 <?php get_header(); ?>
 <h1 class="title-actualites">Nos Actualités</h1>
 <?php if (have_posts()) : ?>
-<div class="container-actu">
-    <?php while (have_posts()) : the_post(); ?>
-        <div class="single-actu">
-            <h2><?php the_title();?></h2>
-            <?php if ( has_post_thumbnail() ) : ?>
-            <img class="main-picture" src="<?php the_post_thumbnail_url('full'); ?>" />
-            <?php endif; ?>
-            <?php echo the_excerpt();?>
-            <a class="read-more" href="<?php the_permalink() ;?>">Lire plus ...</a>
-        </div>
+    <div class="container-actu container">
+    <div class="all-actu">
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="single-actu">
+                <h2><?php the_title();?></h2>
+                <?php if ( has_post_thumbnail() ) : ?>
+                <img class="main-picture" src="<?php the_post_thumbnail_url('full'); ?>" />
+                <?php endif; ?>
+                <?php echo the_excerpt();?>
+                <a class="read-more" href="<?php the_permalink() ;?>">Lire plus ...</a>
+            </div>
         <?php endwhile;?>
-        <div class="actu-sidebar">
-            <?php get_sidebar('sidebar') ;?>
-        </div>
+    </div>
+    <aside class="sidebar">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar') ) : ?>
+            <?php endif; ?>
+    </aside>
     </div>
 <?php endif;?>
 
--- wp-content/themes/pazpok/single.php
+++ wp-content/themes/pazpok/single.php
@@ -3,6 +3,16 @@
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
         <h1 class="title-proprietes"><?php the_title();?></h1>
+        <div class="article container">
+            <div class="article-content">
+                <?php the_content() ;?>
+            </div>
+            <aside class="sidebar">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar') ) : ?>
+                <?php endif; ?>
+            </aside>
+            <?php comments_template('/comments.php', false) ;?>
+        </div>
     <?php endwhile;?>
 <?php endif;?>
 
